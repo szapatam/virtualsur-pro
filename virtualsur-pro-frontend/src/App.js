99ddef5cb52d4510725e52358e79ed5b4a10b9c3
Sebastian Zapata <szapatam96@gmail.com>
2024-10-22 05:24:10 -0300

    Commit #4
    Restructura de archivos

--- virtualsur-pro-frontend/src/App.js
+++ virtualsur-pro-frontend/src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import React from 'react';
 import Layout from './components/layout/layout';
-import ClientList from './components/clientList/ClientList';
+import ClientList from './components/pages/clients/ClientList/ClientList'
 import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/home/Home';
 
 //ESTE COMENTARIO SERA UNA PRUEBA PARA FORK GIT
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<div className="App-content"><h1>Bienvenido a VirtualSur Pro</h1></div>} />
-        <Route path="/clientes" element={<div className='App-content'><ClientList /></div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/clientes" element={<ClientList />} />
       </Routes>
     </Layout>
   );
---
+++ virtualsur-pro-frontend/src/components/pages/home/Home.js
@@ -0,0 +1,15 @@
+// src/pages/home/Home.js
+
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home-container">
+            <h1>Bienvenido a VirtualSur Pro</h1>
+            <p>Gestiona tus eventos de manera eficiente y organizada.</p>
+        </div>
+    );
+}
+
+export default Home;
