44e21969bedd8e1d5920da9d7cf7dc9aa5c8e279
Sebastian Zapata <szapatam96@gmail.com>
2024-10-22 04:01:35 -0300

    commit #2
    prueba fork git

--- virtualsur-pro-frontend/src/App.js
+++ virtualsur-pro-frontend/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import React from 'react';
 import Layout from './components/layout/layout';
-import ClientList  from './components/clientList/ClientList';
+import ClientList from './components/clientList/ClientList';
 import { Routes, Route } from 'react-router-dom';
 
+//ESTE COMENTARIO SERA UNA PRUEBA PARA FORK GIT
 function App() {
   return (
-      <Layout>
-        <Routes>
-          <Route path="/" element={ <div className="App-content"><h1>Bienvenido a VirtualSur Pro</h1></div>} />
-          <Route path="/clientes" element={<div className='App-content'><ClientList /></div>} />
-        </Routes>
-     </Layout>
-  ); 
-} 
- 
+    <Layout>
+      <Routes>
+        <Route path="/" element={<div className="App-content"><h1>Bienvenido a VirtualSur Pro</h1></div>} />
+        <Route path="/clientes" element={<div className='App-content'><ClientList /></div>} />
+      </Routes>
+    </Layout>
+  );
+}
+
 export default App;
