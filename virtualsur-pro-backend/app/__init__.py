154dab8f03028f32bad9c9d61dd580e44c642451
Sebastian Zapata <szapatam96@gmail.com>
2024-11-18 20:36:24 -0300

    commit#19
    cambios repositorio

--- virtualsur-pro-backend/app/__init__.py
+++ virtualsur-pro-backend/app/__init__.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from .routes import main
+from flask_sqlalchemy import SQLAlchemy
+
+# instancia de SQLAlchemy
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
 
+    # Configuración para la base de datos MySQL
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:admin@localhost/virtualsurpro'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'clave123'
 
-    #Config. adicionales
+    db.init_app(app)
+
+    # Config. adicionales
     app.register_blueprint(main)
 
     return app
